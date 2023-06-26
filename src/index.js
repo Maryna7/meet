c78f2bba8faf903acdd4d9f6f14234476c3d867b
Maryna7 <m.kurman23@gmail.com>
2023-06-26 11:38:01 +0200

    integration Atatus into the project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom'; import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('2df1805bec4443dab85fbd9157344dd5').install();
 
 const root = document.getElementById('root');
 
